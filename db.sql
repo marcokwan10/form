8465a0a23f6996959c11e3c20690f06a5ff284f9
marcokwan10 <90655216+marcokwan10@users.noreply.github.com>
2022-04-10 15:00:14 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE canoe;
 
 CREATE TABLE form(
   form_id SERIAL PRIMARY KEY,
-  course_a VARCHAR(32) NOT NULL,
+  course_a VARCHAR(32),
   course_b VARCHAR(32),
   course_c VARCHAR(32)
 );
